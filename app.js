96df07607357c35df3b070eb76ac501aae015e90
3000b <bjoern_xx@gmx.de>
2023-01-20 22:11:57 +0100

    added view object

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict'
+const view = {
+    displayMessage: (msg) => {
+        const messageArea = document.getElementById('messageArea');
+        messageArea.innerHTML = msg;
+    },
+
+    displayHit: (location) => {
+        let cell = document.getElementById(location);
+        cell.setAttribute('class', 'hit')
+    },
+
+    displayMiss: (location) => {
+        let cell = document.getElementById(location);
+        cell.setAttribute('class', 'miss')
+    }
+}
